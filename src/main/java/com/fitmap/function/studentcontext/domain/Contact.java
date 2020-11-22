97d8c2a003d8abd5c46f65075a0b00b06d8eeae7
Danilo F. N. Moreira <danilofnmoreira@gmail.com>
2020-11-22 17:56:40 -0300

    student domain

---
+++ src/main/java/com/fitmap/function/studentcontext/domain/Contact.java
@@ -0,0 +1,115 @@
+package com.fitmap.function.studentcontext.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fitmap.function.studentcontext.v1.payload.request.CreateRequestDtos;
+import com.fitmap.function.studentcontext.v1.payload.request.EditRequestDtos;
+import com.google.cloud.firestore.annotation.DocumentId;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = SnakeCaseStrategy.class)
+@JsonInclude(value = Include.NON_ABSENT)
+public class Contact {
+
+    @NotBlank
+    @DocumentId
+    private String id;
+
+    @Size(max = 500)
+    private String name;
+
+    @Email
+    @Size(max = 500)
+    private String email;
+
+    @Size(max = 50)
+    private String phone;
+
+    @JsonProperty(value = "is_whatsapp")
+    private Boolean whatsapp;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Contact other = (Contact) obj;
+        if (id == null) {
+            return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public static Contact from(CreateRequestDtos.Contact dto) {
+
+        return Contact
+            .builder()
+            .name(dto.getName())
+            .email(dto.getEmail())
+            .phone(dto.getPhone())
+            .whatsapp(dto.getWhatsapp())
+            .build();
+    }
+
+    public static Contact from(EditRequestDtos.Contact dto) {
+
+        return Contact
+            .builder()
+            .id(dto.getId())
+            .name(dto.getName())
+            .email(dto.getEmail())
+            .phone(dto.getPhone())
+            .whatsapp(dto.getWhatsapp())
+            .build();
+    }
+
+    public static <T> List<Contact> from(Collection<T> dtos, Function<T, Contact> mapper) {
+
+        return dtos
+            .stream()
+            .filter(Objects::nonNull)
+            .map(mapper)
+            .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+}
---
+++ src/main/java/com/fitmap/function/studentcontext/domain/Student.java
@@ -0,0 +1,111 @@
+package com.fitmap.function.studentcontext.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fitmap.function.studentcontext.v1.payload.request.CreateRequestDtos;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.cloud.firestore.annotation.Exclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = SnakeCaseStrategy.class)
+@JsonInclude(value = Include.NON_ABSENT)
+public class Student {
+
+    @NotBlank
+    @DocumentId
+    private String id;
+
+    @NotNull
+    @PastOrPresent
+    private Date createdAt;
+
+    @NotNull
+    @PastOrPresent
+    private Date updatedAt;
+
+    @Getter(onMethod = @__({ @Exclude }))
+    @Builder.Default
+    private List<@NotNull Contact> contacts = new ArrayList<>();
+
+    @Getter(onMethod = @__({ @Exclude }))
+    @Builder.Default
+    private List<@NotNull Address> addresses = new ArrayList<>();
+
+    public void addContacts(List<Contact> contacts) {
+
+        var newContacts = Objects.requireNonNullElse(contacts, new ArrayList<Contact>());
+
+        this.contacts = Objects.requireNonNullElse(this.contacts, new ArrayList<Contact>());
+
+        this.contacts.addAll(newContacts);
+    }
+
+    public void addAddresses(List<Address> addresses) {
+
+        var newAddresses = Objects.requireNonNullElse(addresses, new ArrayList<Address>());
+
+        this.addresses = Objects.requireNonNullElse(this.addresses, new ArrayList<Address>());
+
+        this.addresses.addAll(newAddresses);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Student other = (Student) obj;
+        if (id == null) {
+            return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public static Student from(CreateRequestDtos.Student dto, String id) {
+
+        return Student
+            .builder()
+            .id(id)
+            .contacts(Contact.from(dto.getContacts(), Contact::from))
+            .addresses(Address.from(dto.getAddresses(), Address::from))
+            .build();
+    }
+
+}
