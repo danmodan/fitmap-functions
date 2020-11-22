1264c7558d8955a2d67ffed97986238760766093
Danilo F. N. Moreira <danilofnmoreira@gmail.com>
2020-11-22 10:09:41 -0300

    improved sesrvice script

--- service.sh
+++ service.sh
@@ -11,18 +11,29 @@ run_maven() {
                maven:3-adoptopenjdk-11-openj9 $@
 }
 
+deploy_http_function() {
+
+    gcloud functions deploy $1 \
+                            --region=southamerica-east1 \
+                            --entry-point $2 \
+                            --runtime java11 \
+                            --trigger-http \
+                            --max-instances 80 \
+                            --set-env-vars=GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT
+}
+
 case $1 in
     check-deploy)
         gcloud meta list-files-for-upload
         ;;
-    deploy-http-function)
-        gcloud functions deploy $2 \
-                                --region=southamerica-east1 \
-                                --entry-point $3 \
-                                --runtime java11 \
-                                --trigger-http \
-                                --max-instances 80 \
-                                --set-env-vars=GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT
+    deploy-v1-set-roles)
+        deploy_http_function v1-set-roles com.fitmap.function.setroles.v1.SetRolesFunction
+        ;;
+    deploy-v1-gym-crud-gym)
+        deploy_http_function v1-gym-crud-gym com.fitmap.function.gymcontext.v1.CrudGymFunction
+        ;;
+    deploy-v1-gym-crud-contacts)
+        deploy_http_function v1-gym-crud-contacts com.fitmap.function.gymcontext.v1.CrudContactFunction
         ;;
     *)
         echo -e "Invalid option"
